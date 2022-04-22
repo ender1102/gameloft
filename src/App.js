07c8f755783fc48e4589d2e1d6302bd381330939
E N D E R <enderit1102@gmail.com>
2022-04-23 03:14:56 +0700

    final-2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet-async';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import 'animate.css';
 import ContactForm from './screens/ContactForm';
@@ -9,12 +9,21 @@ import Footer from './screens/Footer'
 import Community from './screens/Community';
 import Event from './screens/Event'
 import ExclusiveContent from './screens/ExclusiveContent'
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
+/* 
+███████╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔════╝████╗  ██║██╔══██╗██╔════╝██╔══██╗
+█████╗  ██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+██╔══╝  ██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+███████╗██║ ╚████║██████╔╝███████╗██║  ██║
+╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝ 
+*/
+
 function App() {
-  const isMobile = useMediaQuery({ query: `(max-width: 480px)` });
-  const [control, setControl] = useState('')
+  /* const isMobile = useMediaQuery({ query: `(max-width: 480px)` });
+   const [control, setControl] = useState('')
 
   useEffect(() => {
     if (isMobile){
@@ -22,7 +31,7 @@ function App() {
     }else{
       setControl('full-page')
     }
-  }, [isMobile]);
+  }, [isMobile]); */
 
   return (
     <>
